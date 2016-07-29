fe275cb9657b61fa02a1f938f93281d1c2372041
Greg Brunk <greg.brunk@gmail.com>
2016-07-29 15:35:48 -0600

    Sets up user auth, signup and login routes

--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root to: "users#index"
+	get "/users", to: "users#index", as: "users"
+	get "/users/new", to: "users#new", as: "new_user"
+	post "/users", to: "users#create"
+	get "/users/:id", to: "users#show"
+	get "/sign_in", to: "sessions#new"
+	post "/sessions", to: "sessions#create"
 end
